78857424ee3ea0aa9e4c5ec18b96292232e2d3ea
donaskwhy <donaskwhy@nate.com>
2019-03-05 04:44:39 +0900

    1-24

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from nomadgram.users import models as user_models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+class Comment(TimeStampedModel):
+
+    """ Comment Model """
+
+    message = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT)
+
+
+class Like(TimeStampedModel):
+    
+    """ Like Model """
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT)
--- nomadgram/users/admin.py
+++ nomadgram/users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-
 from nomadgram.users.forms import UserChangeForm, UserCreationForm
 
 User = get_user_model()
